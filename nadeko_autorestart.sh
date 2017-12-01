13a1f6f32a27e1a466a559ca46b562dce8131eca
Matt Burchett <matt@mattburchett.com>
2017-11-30 20:32:13 -0600

    updating to new file

--- nadeko_autorestart.sh
+++ nadeko_autorestart.sh
@@ -14,16 +14,18 @@ mv -n /opt/NadekoBot/src/NadekoBot/credentials.json /root/nadeko/credentials.jso
 rm /opt/NadekoBot/src/NadekoBot/credentials.json > /dev/null 2>&1
 ln -s /root/nadeko/credentials.json /opt/NadekoBot/src/NadekoBot/credentials.json > /dev/null 2>&1
 
-#echo ""
-#echo "Linking Nadeko Data Folder"
-#mkdir -p /root/nadeko/data
-#rsync --ignore-existing -rt /opt/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/ /root/nadeko/data/
-#rm -rf /opt/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/ > /dev/null 2>&1
-#ln -s /root/nadeko/data/ /opt/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data > /dev/null 2>&1
+echo ""
+echo "Patching Nadeko Data Folder"
+mkdir -p /root/nadeko/patch
+cd /root/nadeko/patch && bash "./patch.sh"
+
+echo ""
+echo "Starting Redis-Server"
+/usr/bin/redis-server --daemonize yes
 
 echo ""
 echo "Running NadekoBot with auto restart Please wait."
-cd NadekoBot/src/NadekoBot
+cd /opt/NadekoBot/src/NadekoBot
 while :; do dotnet run -c Release; sleep 5s; done
 echo "Done"
 
